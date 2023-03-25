1d05730faf1a3cff1d0db3d32afe936da0985815
Harine M <haariinee@gmail.com>
2023-03-25 19:06:01 +0530

    minor updates + added exception handling for more error sources -> stop process if script was detected + more lint removal + cleaner outputs

--- data_collection_script.py
+++ data_collection_script.py
@@ -42,69 +42,80 @@ def imslp_safe(url):
 def get_pdf(composer, compositions):
     # set up Chrome driver options
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")  # run the browser in the background
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
     chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-    
+
     # set up Chrome driver with the path to the chromedriver executable
     driver_path = r'C:\Users\rin\Documents\chromedriber\chromedriver.exe'
     driver = webdriver.Chrome(driver_path, options=chrome_options)
-
-    # hopefully circumvent google bot warning
-    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-    driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
-    print(driver.execute_script("return navigator.userAgent;"))
 
     for composition_name in compositions:
         query = f'site:imslp.org {composition_name}'
         query = query.replace(" ", "%20")
         url = f'https://www.google.com/search?q={query}'
-        time.sleep(10)
+        time.sleep(5)
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # get the first IMSLP link
-        imslp_link = soup.find('a', href=lambda href: href and
-                               composer in href.lower())['href']
+        try:
+            imslp_link = soup.find('a', href=lambda href: href and composer in href.lower() and href[:24]=="/url?q=https://imslp.org")['href']
+        except:
+            if "unusual traffic" in response.text:
+                print(f"{composer}- {composition_name}\t|\tUnusual traffic detected. Stopping process.")
+                break
+            else:
+                print(f"{composer}- {composition_name}\t|\tSite not found. Skipping")
+                continue
         valid_link = imslp_link.split("&sa")
         valid_link = imslp_safe(valid_link[0][7:])
+        time.sleep(2)
         driver.get(valid_link+"#tabScore1")
-         
+
+        if "site ripping ban" in driver.find_element(By.TAG_NAME, "body").text:
+            time_now = time.strftime("%H:%M:%S", time.localtime())
+            print(f"{composer}- {composition_name}\t|\tBan applied at IMSLP @ {time_now}")
+            break
+
         '''
         Search for "Complete Score (scan) and go to the link containing it"
         In case of no cookies/fresh session, a disclaimer page will pop up. If statement will handle it.
         Wait 15 seconds for download link to be generated, additional 2 seconds for buffer
         '''
-        print(driver.title)
         try:
             driver.find_element('xpath', "//a[.//span[text()='Complete Score (scan)']]").click()
         except:
             try:
                 driver.find_element('xpath', "//a[.//span[text()='Complete Score']]").click()
             except:
-                print(f'{composition_name} complete score not found, skipping')
+                print(f'{composer}- {composition_name}\t|\tComplete score not found, skipping')
                 continue
-            
-        print("Complete score found")
+
         if driver.title == "Disclaimer - IMSLP: Free Sheet Music PDF Download":
             driver.find_element(By.LINK_TEXT, "I accept this disclaimer, continue to download file").click()
-        time.sleep(17)
-        download_link = driver.find_element('xpath', "//a[.='Click here to continue your download.']")
+        elif driver.title != "Subscribe - IMSLP: Free Sheet Music PDF Download":
+            print(f'{composer}- {composition_name}\t|\t{driver.title} page encountered. Skipping.')
+            break
+        time.sleep(20)
+
+        try:
+            download_link = driver.find_element('xpath', "//a[.='Click here to continue your download.']")
+        except:
+            print(f'{composer}- {composition_name}\t|\tDownload link not found.')
+            break
         pdf_url = download_link.get_attribute('href')
-        print("Download link found:", pdf_url)
         response = requests.get(pdf_url)
         with open(f'{path}{composer}\\{composition_name}.pdf', 'wb') as f:
-            print("Downloading")
             f.write(response.content)
 
         to_image(composer, composition_name)
-        print("Converted", composition_name, "to image.")
+        print(f'{composer}- {composition_name}\t|\t Downloaded and converted to image.')
     # quit the browser
     driver.quit()
 
 
 # run the script simulaneously for each composer
-threads = [Thread(target= get_pdf, args=(c, compositions[c], )) for c in compositions]
+threads = [Thread(target = get_pdf, args=(c, compositions[c], )) for c in compositions]
 for t in threads:
     t.start()
 
